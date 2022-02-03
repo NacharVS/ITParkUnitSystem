665e9acb90ce222d6af99ebb0a0a899192a48dc5
nacharvs <r3dinc@yandex.ru>
2022-02-03 18:31:23 +0300

    start

---
+++ UnitImplementation/IUnit.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace UnitImplementation
+{
+    public interface IUnit
+    {
+    }
+}
---
+++ Units/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnitImplementation;
+
+namespace Units
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                    
+        }
+    }
+}
