f24cfa3010e0a9d9766b5bdd9cff2aeb82ab8aa7
AndreiZasuhin <Bitolborg@yandex.ru>
2022-02-10 20:19:26 +0300

    mongoClassadd

---
+++ Units/MongoDataBase.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+
+namespace Units
+{
+    class MongoDataBase
+    {
+        public static void AddArcherToDataBase(Archer archer)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("ITParkUnitSystem");
+            var collection = database.GetCollection<Archer>("RangeUnits");
+            collection.InsertOne(archer);
+        }
+
+        public static void AddFootmanToDataBase(Footman footman)
+        {
+            MongoClient client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("ITParkUnitSystem");
+            var collection = database.GetCollection<Footman>("RangeUnits");
+            collection.InsertOne(footman);
+        }
+    }
+}
