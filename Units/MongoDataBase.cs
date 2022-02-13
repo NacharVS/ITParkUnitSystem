8197c077e0a6f4e266d2757ee1cc1584ceb278f5
DESKTOP-F8PTRBK\Нияз <kamalov.niaz@mail.ru>
2022-02-13 18:06:53 +0300

    Mongodb

---
+++ Units/MongoDataBase.cs
@@ -0,0 +1,16 @@
+﻿
+using MongoDB.Driver;
+
+namespace Units
+{
+    class MongoDataBase
+    {
+        public static void AddArcherToDataBase(Archer archer)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("ITUnitSystem");
+            var collection = database.GetCollection<Archer>("RangeUnits");
+            collection.InsertOne(archer);
+        }
+    }
+}
